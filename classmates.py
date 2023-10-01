aa553da6940e6b80f68e2f6bc29e1c6d7c9aeafb
Dongli Liu <dongliliu0@gmail.com>
2023-10-01 12:58:22 -0700

    added classmates, node.py

--- classmates.py
+++ classmates.py
@@ -5,9 +5,6 @@ Created on Sun Oct  1 12:12:15 2023
 @author: Dongli
 """
 
-import networkx as nx
-import matplotlib.pyplot as plt
-
 relationships = {}
 
 relationships['Dongli'] = ['Adam', 'Frank', 'George']
@@ -18,12 +15,4 @@ relationships['Dolly'] = ['Ema', 'Bob']
 relationships['Frank'] = ['Dongli']
 relationships['George'] = ['Dongli']
 
-# Create a directed graph from the dictionary
-G = nx.DiGraph(relationships)
-
-# Draw the graph
-pos = nx.spring_layout(G, seed=42)  # Define a layout for the nodes
-nx.draw(G, pos, with_labels=True, node_size=500, node_color='skyblue', font_size=10, font_color='black', font_weight='bold', arrowsize=20)
-
-# Show the plot
-plt.show()
+print(relationships)
